baff2d137a685ee16b947379f841b8df0074e8f6
IsolinaBurjel <Margaritaburjel@gmail.com>
2023-11-24 19:41:17 -0300

    FirstCOmmit SponsorsLogos

--- src/app/core/components/sponsor-listado/sponsor-listado.component.ts
+++ src/app/core/components/sponsor-listado/sponsor-listado.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SponsorService } from '../../services/sponsor.service';
+import { Sponsor } from '../../models/sponsor';
 
 @Component({
   selector: 'app-sponsor-listado',
   templateUrl: './sponsor-listado.component.html',
   styleUrls: ['./sponsor-listado.component.css']
 })
-export class SponsorListadoComponent {
+export class SponsorListadoComponent implements OnInit{
+  data!:Sponsor[];
+  constructor(private sponsorsService:SponsorService){}
+  ngOnInit(): void {
+    this.obtenerData();
+  }
+
+  obtenerData():void{
+    this.sponsorsService.obtenerSponsors().subscribe((data)=>this.data=data as Sponsor[]);
+  }
 
 }
+
+
